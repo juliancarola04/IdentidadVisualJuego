b9c02897439aea54336436291b023ad05a8ea825
Julian Carola <juliancarola04@gmail.com>
2025-11-04 15:41:24 -0300

    Modificar propiedades de las clases y agregar comentarios explicando el funcionamiento. También agregar código en la pantalla del juego para el establecimiento inicial de una partida.

--- js/appPantallaPartida.js
+++ js/appPantallaPartida.js
@@ -1,20 +1,40 @@
 import { Jugador, Tablero } from "./clases/clases.js";
 
-const JSONdePartida = JSON.parse(localStorage.getItem("partidaJSON"));
+const JSONdeTablero = JSON.parse(localStorage.getItem("tableroJSON"));
 const JSONdeJugadores = JSON.parse(localStorage.getItem("nombreJugadoresJSON"));
 
-// Si no existe ninguna partida guardada
-if (JSONdePartida === null)
+let jugador1;
+let jugador2;
+let tablero;
+
+// Si no existe ningún tablero almacenado
+if (JSONdeTablero === null)
 {
     // Y no hay ningún jugador almacenado
     if (JSONdeJugadores === null){
         // Se va
+        console.log("No hay nada de nada almacenado")
         window.location.href = "../html/index.html";
     }
 
-    // Si hay jugadores almacenados crea la partida con los jugadores
-} 
+    // Y hay jugadores almacenados, crea el tablero con los jugadores
+    console.log("No hay una partida almacenada")
 
-// Si hay una partida guardada, parte desde esa partida.
+    jugador1 = new Jugador(JSONdeJugadores[0]);
+    jugador2 = new Jugador(JSONdeJugadores[1]);
+    tablero = new Tablero(jugador1, jugador2);
 
+    localStorage.removeItem("nombreJugadoresJSON"); // Acá elimino el localStorage de los jugadores ya que no va a ser necesario
+    localStorage.setItem("tableroJSON", JSON.stringify(tablero)); // Acá guardo en el localStorage el estado inicial del tablero (se va a ir actualizando después).
 
+} else {
+
+    // Si hay un tablero almacenado, parte desde ahí.
+    console.log("Hay una partida almacenada")
+
+    const datosPartida = JSONdeTablero;
+
+    jugador1 = new Jugador(datosPartida.jugadorUno.nombre, datosPartida.jugadorUno.puntajeActual, datosPartida.jugadorUno.puntajeTotal, datosPartida.jugadorUno.rondasGanadas);
+    jugador2 = new Jugador(datosPartida.jugadorDos.nombre, datosPartida.jugadorDos.puntajeActual, datosPartida.jugadorDos.puntajeTotal, datosPartida.jugadorDos.rondasGanadas);
+    tablero = new Tablero(jugador1, jugador2, datosPartida.rondaActual, datosPartida.resultadoDado);
+}
--- js/clases/clases.js
+++ js/clases/clases.js
@@ -1,17 +1,41 @@
 // Para ver el tema de las clases: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
 
 export class Jugador {
-    #puntajeActual = 0;
-    #puntajeTotal = 0;
-    #rondasGanadas = 0;
+    // Lo ideal sería poner estas propiedades en privado y después modificar sus valores con los setters,
+    // pero hay problemas con el JSON si se hace. Investigar un poco más después.
+    puntajeActual;
+    puntajeTotal;
+    rondasGanadas;
     
-    constructor (nombre){
+    // Sol, JS a diferencia de C# no puede tener varios constructores, así que para que después se pueda utilizar esta clase
+    // a la hora de empezar una partida con datos ya guardados tengo que ponerle varios parámetros que por defecto empiezan en 0. 
+    // Cuando se pasa un valor correspondiente a esos campos se cambia el valor por defecto al que se pasó.  
+    constructor (nombre, puntajeActual = 0, puntajeTotal = 0, rondasGanadas = 0){
         this.nombre = nombre;
+        this.puntajeActual = puntajeActual;
+        this.puntajeTotal = puntajeTotal;
+        this.rondasGanadas = rondasGanadas;
     }
 
+    // Para ver el tema de los getters: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/get
+    // La diferencia es que no requieren llamarlos como si fueran métodos (más parecido a las propiedades de C#)
     get getNombre(){
         return this.nombre;
     }
+
+    // Para ver el tema de los setters: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Functions/set
+
+    set setPuntajeActual(puntajeActual){
+        this.puntajeActual = puntajeActual;
+    }
+
+    set setPuntajeTotal(puntajeTotal){
+        this.puntajeTotal = puntajeTotal;
+    }
+    
+    set setRondasGanadas(rondasGanadas){
+        this.rondasGanadas = rondasGanadas;
+    }    
 
     // Para ver el tema del random: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
     tirarDado(){
@@ -20,11 +44,13 @@ export class Jugador {
 }
 
 export class Tablero {
-    #rondaActual = 0;
-    #resultadoDado = 0;
+    rondaActual;
+    resultadoDado;
 
-    constructor (jugadorUno, jugadorDos){
+    constructor (jugadorUno, jugadorDos, rondaActual = 0, resultadoDado = 0){
         this.jugadorUno = jugadorUno;
         this.jugadorDos = jugadorDos;
+        this.rondaActual = rondaActual;
+        this.resultadoDado = resultadoDado
     }
 }
